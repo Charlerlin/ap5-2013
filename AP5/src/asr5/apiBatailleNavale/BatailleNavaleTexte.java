24b42f90815f5e755d7c5be089ff967883541943
Charlerlin <charlerlin@gmail.com>
2013-03-04 16:49:01 +0100

    Commit the new TP about the API "Bataille Navale"

---
+++ AP5/src/asr5/apiBatailleNavale/BatailleNavaleTexte.java
@@ -0,0 +1,29 @@
+package asr5.apiBatailleNavale;
+
+import batailleNavale.modele.BatailleNavale;
+
+/**
+ * 
+ * @author charlerlin
+ *
+ */
+
+public class BatailleNavaleTexte {
+	
+	public static void main(String[] args) {
+		BatailleNavale jeu = new BatailleNavale(10, 10);
+		JoueurRandom batman = new JoueurRandom("Batman", jeu);
+		JoueurRandom pacman = new JoueurRandom("Pacman", jeu);
+		jeu.addJoueurs( batman, pacman);
+		jeu.ajouteBateauxRandom(0);
+		jeu.ajouteBateauxRandom(1);
+		System.out.println(jeu);
+		while(jeu.gagnant()==null){
+			batman.joue();
+			pacman.joue();
+			System.out.println(jeu);
+		}
+		System.out.println(jeu.gagnant());
+		
+	}
+}
---
+++ AP5/src/asr5/apiBatailleNavale/JoueurRandom.java
@@ -0,0 +1,33 @@
+package asr5.apiBatailleNavale;
+
+import batailleNavale.modele.BatailleNavale;
+import batailleNavale.modele.CaseNotFoundException;
+import batailleNavale.modele.Joueur;
+
+/**
+ * 
+ * @author charlerlin
+ *
+ */
+
+public class JoueurRandom extends Joueur{
+
+	public JoueurRandom(String name, BatailleNavale plateau) {
+		super(name, plateau);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void joue() {
+		BatailleNavale bn = this.getModele();
+		int h = (int)(Math.random() * bn.getHauteur());
+		int l = (int)(Math.random() * bn.getLargeur());
+		try {
+			bn.tir(h, l);
+		} catch (CaseNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
